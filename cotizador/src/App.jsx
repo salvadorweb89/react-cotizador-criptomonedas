ee7da9d489f7e61c33c2d76485199359a488b733
salvadorweb89 <salvadorweb89@gmail.com>
2023-09-09 16:59:28 +0200

    - Instalo dependencias emotion/react y emotion/styled para implementar styled-components. - Defino los primeros style-components con sus repectivos estilos.

--- cotizador/src/App.jsx
+++ cotizador/src/App.jsx
@@ -1,8 +1,55 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import styled from '@emotion/styled';
+import ImagenCripto from './img/imagen-criptos.png';
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+
+  @media (min-width: 992px) {
+    column-gap:  2rem;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+  }
+`;
+
+const Heading = styled.h1`
+  color: #FFF;
+  font-family: 'Lato', sans-serif;
+  font-size: 34px;
+  font-weight: 700;
+  margin: 80px 0px 50px 0px;
+  text-align: center;
+
+  &::after {
+    background-color:#66A2FE;
+    content: '';
+    display: block;
+    height: 6px;
+    margin: 10px auto 0 auto;
+    width: 100px;
+  }
+`;
+
+const Imagen = styled.img`
+  display: block;
+  margin: 100px auto 0 auto;
+  max-width: 400px;
+  width: 80%;
+`;
 
 function App() {
   return (
-    <h1>Desde app</h1>
+    <Contenedor>
+      <Imagen 
+        src={ImagenCripto}
+        alt="Imagen Criptomonedas"
+      />
+      <div>
+        <Heading>Cotizador Criptomonedas</Heading>
+      </div>
+    </Contenedor>
   )
 }
 
